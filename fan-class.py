6a8c88a872e10a5f4c266550d53b193d0398a207
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 01:59:45 +0800

    created class + variables

--- fan-class.py
+++ fan-class.py
@@ -1,3 +1,10 @@
 #create class fan
-#set default values
-#get values
+class fan:
+    #set default values
+    def __init__(self,speed = 'SLOW', radius = 5, power = 0, color ='blue'):
+    #private variables
+        self.__speed = speed
+        self.__radius = radius
+        self.__power = power
+        self.__color = color
+    #get values
