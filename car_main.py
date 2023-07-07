c174bddd6b46a2b99d32793586f17d5edf597712
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 23:18:21 +0800

    typos ulit

--- car_main.py
+++ car_main.py
@@ -6,10 +6,12 @@ vroomvroom = car(None,None)
 #accelerate for five times vroom vroom vroommmm
 result = pyfiglet.figlet_format("Let's ride...", font = "bubble" )
 print(result)
-for i in range (5):
-    vroomvroom.acceleration=()
+for i in range(5):
+    vroomvroom.acceleration()
+    print("\nThe car's current speed is:", vroomvroom.valuereturnspeed())
 #hit the brakes five times stopppp
 result = pyfiglet.figlet_format("Let's slow it down...", font = "bubble")
 print (result)
-for i in range (5):
-    vroomvroom.brakenakamieme=()
+for i in range(5):
+    vroomvroom.brakenakamieme()
+    print("\nThe car's current speed is:", vroomvroom.valuereturnspeed())
