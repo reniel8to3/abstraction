1f612a77f4a76b87bc471e8b865cdb46b8fc180f
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 22:58:20 +0800

    changed fonts

--- car_main.py
+++ car_main.py
@@ -4,12 +4,12 @@ import pyfiglet
 #create car vroom vroom
 vroomvroom = car(None,None)
 #accelerate for five times vroom vroom vroommmm
-result = pyfiglet.figlet_format("Let's ride...", font = "bulbhead" )
+result = pyfiglet.figlet_format("Let's ride...", font = "bubble" )
 print(result)
 for i in range (5):
     vroomvroom.acceleration=()
 #hit the brakes five times stopppp
-result = pyfiglet.figlet_format("Let's slow it down...", font = "bulbhead")
+result = pyfiglet.figlet_format("Let's slow it down...", font = "bubble")
 print (result)
 for i in range (5):
     vroomvroom.brakenakamieme=()
