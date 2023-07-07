3419044ff4decf545e2feb4d035adda3ad725dfe
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 23:19:32 +0800

    added time module (shala)

--- car_main.py
+++ car_main.py
@@ -1,6 +1,7 @@
 #import car
 from car_class import car
 import pyfiglet
+import time
 #create car vroom vroom
 vroomvroom = car(None,None)
 #accelerate for five times vroom vroom vroommmm
@@ -9,9 +10,11 @@ print(result)
 for i in range(5):
     vroomvroom.acceleration()
     print("\nThe car's current speed is:", vroomvroom.valuereturnspeed())
+    time.sleep(0.5)
 #hit the brakes five times stopppp
 result = pyfiglet.figlet_format("Let's slow it down...", font = "bubble")
 print (result)
 for i in range(5):
     vroomvroom.brakenakamieme()
     print("\nThe car's current speed is:", vroomvroom.valuereturnspeed())
+    time.sleep(0.5)
