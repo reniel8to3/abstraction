92819086f9abe41acaf4b45445bf9a84b654241b
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 22:16:09 +0800

    pseudocode

--- fan-main.py
+++ fan-main.py
@@ -1,6 +1,5 @@
 #import class
 from fan_class import fan
-import pyfiglet 
 #create 2 objects
 fan_1 = fan(3, 10, 'yellow', True)
 fan_2 = fan(1, 5, 'blue', False)
@@ -15,4 +14,4 @@ fan_2.fan_speed_setter()
 fan_2.radius_setter()
 fan_2.color_setter()
 fan_2.power_setter()
-print('The speed of Fan 2 is ' ,fan_2.fan_speed_getter(), ' and its radius is ' ,fan_2.radius_getter(), ' while the fan is ' ,fan_2.color_getter(), ' and its color is ' ,fan_2.power_getter(), '.')
+print('The speed of Fan 2 is ' ,fan_2.fan_speed_getter(), ' and its radius is ' ,fan_2.radius_getter(), ' while the fan is ' ,fan_2.color_getter(), ' and its is ' ,fan_2.power_getter(), '.')
