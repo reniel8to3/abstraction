a1df3dc6f8cda077a0bdcebf01362e3eb4fc6129
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 23:31:15 +0800

    pseudocode + constructors

---
+++ pet_class.py
@@ -0,0 +1,11 @@
+#create class
+class pet:
+#create constructors
+    def __init__(self):
+        self.__name =""
+        self.__animal_type=""
+        self.__age=0
+#ask for pet name
+#ask for animal type
+#ask for pet age
+#return name, type and age
