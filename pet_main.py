ec678eb711be5ef688b25b150a491ec158f1dc4e
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-08 00:10:26 +0800

    typo (im sleepy)

--- pet_main.py
+++ pet_main.py
@@ -9,9 +9,8 @@ pet_meowarf.set_name
 pet_meowarf.set_animal_type
 pet_meowarf.set_age
 #return all pet data
-meow = pyfiglet.figlet_format("processing...", font = "linux" )
+meow = pyfiglet.figlet_format("processing", font = "linux" )
 print(meow)
-
 print("Your pet's name is: ", pet_meowarf.get_name())
 time.sleep(0.5)
 print("Your pet is a/an: ",pet_meowarf.get_animal_type())
