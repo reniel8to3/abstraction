7867e9d98edae4e555ef7dd41560a212c05ca876
reniel_8to3 <renielalmariomalabo@gmail.com>
2023-07-07 22:56:57 +0800

    added car, accele (i forgot how to spell), etc

--- car_main.py
+++ car_main.py
@@ -1,5 +1,15 @@
 #import car
+from car_class import car
+import pyfiglet
 #create car vroom vroom
+vroomvroom = car(None,None)
 #accelerate for five times vroom vroom vroommmm
+result = pyfiglet.figlet_format("Let's ride...", font = "bulbhead" )
+print(result)
+for i in range (5):
+    vroomvroom.acceleration=()
 #hit the brakes five times stopppp
-#get current speed of the car
+result = pyfiglet.figlet_format("Let's slow it down...", font = "bulbhead")
+print (result)
+for i in range (5):
+    vroomvroom.brakenakamieme=()
